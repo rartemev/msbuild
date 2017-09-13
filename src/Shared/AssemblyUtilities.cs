3c52595762a6da7e2e7f560c1dc0609158413725
Roman Artemev <rartemev@microsoft.com>
2017-09-13 15:13:08 -0700

    Backported concurrent collections

--- src/Shared/AssemblyUtilities.cs
+++ src/Shared/AssemblyUtilities.cs
@@ -36,6 +36,16 @@ namespace Microsoft.Build.Shared
 #endif
         }
 
+#if CLR2COMPATIBILITY
+        /// <summary>
+        /// Shim for the lack of <see cref="System.Reflection.IntrospectionExtensions.GetTypeInfo"/> in .NET 3.5.
+        /// </summary>
+        public static Type GetTypeInfo(this Type t)
+        {
+            return t;
+        }
+#endif
+
         public static AssemblyName CloneIfPossible(this AssemblyName assemblyNameToClone)
         {
 #if FEATURE_ASSEMBLYNAME_CLONE
