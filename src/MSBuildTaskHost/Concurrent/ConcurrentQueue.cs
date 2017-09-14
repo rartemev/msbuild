5b0983077386ad8a7d2bf1816429e87d72e6e842
Roman Artemev <rartemev@microsoft.com>
2017-09-13 17:36:16 -0700

    Fixed attributes

--- src/MSBuildTaskHost/Concurrent/ConcurrentQueue.cs
+++ src/MSBuildTaskHost/Concurrent/ConcurrentQueue.cs
@@ -545,14 +545,14 @@ namespace Microsoft.Build.Shared.Concurrent
                 /// <summary>The sequence number for this slot, used to synchronize between enqueuers and dequeuers.</summary>
                 public int SequenceNumber;
             }
-            /// <summary>Padded head and tail indices, to avoid false sharing between producers and consumers.</summary>
-            [DebuggerDisplay("Head = {Head}, Tail = {Tail}")]
-            [StructLayout(LayoutKind.Explicit, Size = 192)] // padding before/between/after fields based on typical cache line size of 64
-            internal struct PaddedHeadAndTail
-            {
-                [FieldOffset(64)] public int Head;
-                [FieldOffset(128)] public int Tail;
-            }
         }
+    }
+    /// <summary>Padded head and tail indices, to avoid false sharing between producers and consumers.</summary>
+    [DebuggerDisplay("Head = {Head}, Tail = {Tail}")]
+    [StructLayout(LayoutKind.Explicit, Size = 192)] // padding before/between/after fields based on typical cache line size of 64
+    internal struct PaddedHeadAndTail
+    {
+        [FieldOffset(64)] public int Head;
+        [FieldOffset(128)] public int Tail;
     }
 }
